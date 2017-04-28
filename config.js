c8f3d14218ed4afefe21f788377831366c00876b
Jeremy Black <jb@yodacom.com>
2017-04-28 09:36:24 -0600

    added back server.js and fixed dotenv code

--- config.js
+++ config.js
@@ -3,5 +3,4 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                         (process.env.NODE_ENV === 'Production' ? 
                         'mongodb://localhost/shopping-list':
                         'mongodb://localhost/shopping-list-dev');
-                        // "mongodb://jbyodacom:fr4gt5%@ds119091.mlab.com:19091/shopping-list");
 exports.PORT = process.env.PORT || 8080;
--- server.js
+++ server.js
@@ -1,37 +1,39 @@
 var express = require("express");
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+require ("dotenv").config();
 
 var config = require("./config");
 
 var app = express();
 
 app.use(bodyParser.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 var runServer = function(callback) {
-    mongoose.connect(config.DATABASE_URL, function(err){
-        if (err && callback) {
-            return callback(err);
-        }
+	console.log(config.DATABASE_URL);
+	mongoose.connect(config.DATABASE_URL, function(err){
+		if (err && callback) {
+			return callback(err);
+		}
         
-        app.listen(config.PORT, function(){
-           console.log('listening on localhost:' + config.PORT);
-           if (callback) {
-               callback();
-           }
+		app.listen(config.PORT, function(){
+			console.log("listening on localhost:" + config.PORT);
+			if (callback) {
+				callback();
+			}
            
-        });
+		});
 
-    });
+	});
 };
 
 if (require.main === module) {
-    runServer(function (err) {
-        if (err) {
-            console.error(err);
-        }
-    });
+	runServer(function (err) {
+		if (err) {
+			console.error(err);
+		}
+	});
 }
 
 
@@ -39,35 +41,35 @@ if (require.main === module) {
 var Item = require("./models/item");
 
 app.get("/items", function(req, res){
-    Item.find(function(err,items){
-        if(err){
-            return res.status(500).json({
-                message: "internal Server Error"
-            });
-        }
-        res.json(items);
-    });
+	Item.find(function(err,items){
+		if(err){
+			return res.status(500).json({
+				message: "internal Server Error"
+			});
+		}
+		res.json(items);
+	});
 });
 
 app.post("/items" , function(req,res){
-    Item.create({
-        name:req.body.name
-    }, function(err, item) {
-        if (err) {
-            return res.status(500).json({
-                message:"Internal Server Error"
-           });
-        }
-    res.status(201).json(item);
+	Item.create({
+		name:req.body.name
+	}, function(err, item) {
+		if (err) {
+			return res.status(500).json({
+				message:"Internal Server Error"
+			});
+		}
+		res.status(201).json(item);
     
-    });
+	});
 });
 
 
 app.use("*", function(req,res){
-    res.status(404).json({
-        message: "Not Found"
-    });
+	res.status(404).json({
+		message: "Not Found"
+	});
 });
 
 
